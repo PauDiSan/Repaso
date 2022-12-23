a67117f162e85e156c95c538621f621dfe529af7
PauDiSan <paula.diazsantos@iesmiguelherrero.com>
2022-12-23 18:49:32 +0100

    Primer commit

---
+++ ProyectoRepaso/src/main/java/ejercicios/proyectorepaso/ProyectoRepaso.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Project/Maven2/JavaApp/src/main/java/${packagePath}/${mainClassName}.java to edit this template
+ */
+
+package ejercicios.proyectorepaso;
+
+/**
+ *
+ * @author DAM111
+ */
+public class ProyectoRepaso {
+
+    public static void main(String[] args) {
+        System.out.println("Repaso día 23/12/2022");
+    }
+}
